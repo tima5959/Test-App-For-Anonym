7eb68a8fb232609e4f5f245898b9718e81d8c19c
tima5959 <timurfatullaev98@gmail.com>
2020-09-21 22:51:40 +0300

    Initial Commit

---
+++ Test App For Anonym/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test App For Anonym
+//
+//  Created by Тимур Фатулоев on 21.09.2020.
+//  Copyright © 2020 Тимур Фатулоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
